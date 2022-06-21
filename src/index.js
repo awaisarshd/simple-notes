214263dc811a975efbeef7093796cb72e11c37c3
Awais Arshad <awais.arshad@outlook.com>
2022-06-21 10:36:09 +0400

    Implemeted redux store

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React, { lazy, Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
-import reportWebVitals from "./reportWebVitals";
 import { Spinner } from "reactstrap";
 import { Slide, ToastContainer } from "react-toastify";
+
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 import "react-toastify/dist/ReactToastify.css";
 
@@ -14,14 +16,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <BrowserRouter>
-        <Suspense fallback={<Spinner />}>
-            <LazyApp />
-            <ToastContainer transition={Slide} />
-        </Suspense>
+        <Provider store={store}>
+            <Suspense fallback={<Spinner />}>
+                <LazyApp />
+                <ToastContainer transition={Slide} />
+            </Suspense>
+        </Provider>
     </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
